2525e62641dc2fb7bf3f6946908c24af23f05684
stephencoyle <steviecoyle@hotmail.com>
2024-04-06 19:45:17 +0100

    Initial Commit

---
+++ src/main/java/com/scoyle/shrtnrjavaspring/ShrtnrJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.scoyle.shrtnrjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrtnrJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrtnrJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scoyle/shrtnrjavaspring/ShrtnrJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scoyle.shrtnrjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrtnrJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
