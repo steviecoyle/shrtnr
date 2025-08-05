846e4357cc23f960658922b96f5032ce6fca3ed5
Stephen Coyle <steviecoyle@hotmail.com>
2025-08-05 23:45:32 +0100

    rename ShrtnrJavaSpringApplication.java to ShrtnrApplication.java

--- src/main/java/com/scoyle/shrtnr/ShrtnrApplication.java
+++ src/main/java/com/scoyle/shrtnr/ShrtnrApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShrtnrJavaSpringApplication {
+public class ShrtnrApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShrtnrJavaSpringApplication.class, args);
+		SpringApplication.run(ShrtnrApplication.class, args);
 	}
 
 }
