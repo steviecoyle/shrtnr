56f447162a6b72f69c8a22d5971177f5c05de937
Stephen Coyle <steviecoyle@hotmail.com>
2025-09-17 22:29:20 +0100

    add UrlShortenerService with empty methods

---
+++ src/main/java/com/scoyle/shrtnr/service/impl/UrlShortenerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.scoyle.shrtnr.service.impl;
+
+import com.scoyle.shrtnr.persistence.repository.ShortUrlRepository;
+import com.scoyle.shrtnr.service.UrlShortenerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Log4j2
+@Service
+class UrlShortenerServiceImpl implements UrlShortenerService {
+
+    private final ShortUrlRepository shortUrlRepository;
+
+    @Override
+    public String getLongUrl(final String shortUrl) {
+        return "";
+    }
+
+    @Override
+    public String saveLogUrl(final String longUrl) {
+        return "";
+    }
+}
