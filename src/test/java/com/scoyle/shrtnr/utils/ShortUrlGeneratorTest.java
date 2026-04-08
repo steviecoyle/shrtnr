2d9db353d3070e607316b2470bdbbea371f27701
Stephen Coyle <steviecoyle@hotmail.com>
2026-04-08 17:07:45 +0100

    refactor logic to generate code to use SecureRandom

--- src/test/java/com/scoyle/shrtnr/utils/ShortUrlGeneratorTest.java
+++ src/test/java/com/scoyle/shrtnr/utils/ShortUrlGeneratorTest.java
@@ -2,37 +2,30 @@ package com.scoyle.shrtnr.utils;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ShortUrlGeneratorTest {
 
     @Test
-    void verify_generated_code_is_not_null() {
-        ShortUrlGenerator generator = ShortUrlGenerator.getInstance();
-
-        String code = generator.encode();
-
-        assertNotNull(code);
-        System.out.println(code);
-    }
-
-    @Test
     void verify_generated_codes() {
         Set<String> codes = new HashSet<>();
-        ShortUrlGenerator generator = ShortUrlGenerator.getInstance();
 
-        for (int i = 0; i < 100; i++) {
-            String code = generator.encode();
+        int maxNumberOfCodes = 1000;
+
+        for (int i = 0; i < maxNumberOfCodes; i++) {
+            String code = ShortUrlGenerator.generateShortCode();
             codes.add(code);
+
+            // assert all codes have a length of 7 characters
+            assertEquals(7, code.length());
+
             System.out.println("No. " + i + ", code = " + code);
         }
 
-        assertEquals(100, codes.size());
+        // assert all codes are generated
+        assertEquals(maxNumberOfCodes, codes.size());
     }
 }
